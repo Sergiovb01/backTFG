0a9fad46c00a2d49818ee21f59663f8ea075043d
Sergio <sergio.vicente-barbera.external@capgemini.com>
2025-04-14 11:07:30 +0200

    React App ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { dbConection } = require('./database/config');
 const cors = require('cors');
@@ -22,6 +23,10 @@ app.use(express.json());
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.get('/*path', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //Escuchar peticiones 
 app.listen(process.env.PORT, () => { //coger el puerto de las variables de entorno
